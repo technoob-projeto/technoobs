765ae80d78e79d043cc88f6e604009cb871c89d6
ericaramos682 <ramossilva682@gmail.com>
2022-07-12 21:55:58 -0300

    feat: Adicionando style para mobile first na page Home

--- technoobs/src/components/Header/styles.js
+++ technoobs/src/components/Header/styles.js
@@ -5,14 +5,24 @@ export const HeaderStyle = styled.ul`
   align-items: center;
   justify-content: space-between;
 
-  width: 97,5%;
+  width: 97, 5%;
   height: 80px;
   padding: 1rem;
- 
 
   background-color: #070707;
 
-  background: linear-gradient(45deg,#632291,#13A7BB,#217698, #2A5581, #33437A, #413A80, #562B8B, #632291, #13A7BB);
+  background: linear-gradient(
+    45deg,
+    #632291,
+    #13a7bb,
+    #217698,
+    #2a5581,
+    #33437a,
+    #413a80,
+    #562b8b,
+    #632291,
+    #13a7bb
+  );
   background-size: 200% 200%;
   animation: colors 22s ease infinite;
 
@@ -30,7 +40,7 @@ export const HeaderStyle = styled.ul`
     }
   }
 
- li{
+  li {
     display: flex;
     justify-content: space-between;
     align-items: center;
@@ -38,10 +48,8 @@ export const HeaderStyle = styled.ul`
 
   .img__logo {
     width: 150px;
-    
   }
   .img__menu {
-    
     width: 50px;
     height: 50px;
   }
@@ -51,16 +59,14 @@ export const HeaderStyle = styled.ul`
     border: none;
     font-size: 42px;
     font-weight: 900;
-    color: #E9EAF1;
-    font-family: 'VT323', monospace;
-    &:hover{
-      &:hover{
-        
+    color: #e9eaf1;
+    font-family: "VT323", monospace;
+    &:hover {
+      &:hover {
         color: #7fff00;
         box-shadow: 1px 2px 50px #7fff00;
+      }
     }
-    }
-   
   }
 
   .btn__login {
--- technoobs/src/pages/Home/styles.js
+++ technoobs/src/pages/Home/styles.js
@@ -2,13 +2,13 @@ import styled from "styled-components";
 
 export const HomeStyle = styled.main`
   width: 100%;
-  height: 1000px;
+  height: 40rem;
   background-color: #002a32;
   font-family: "VT323", monospace;
   background-image: url(https://s3-alpha-sig.figma.com/img/83f4/a1d3/8642817e7d3ef59623c7160eca36401f?Expires=1658707200&Signature=ddK62VN2DvhJiGyQkq9STInfbcl-zptwhTp39RZ5dJTzPd0IOvvsbSTsEv9FUe4~-zSkRYhMoeah8K-DGxSbEGqn7~CWXQfBlb8syJX7U5ljWkUGzObPHifZWt-lbJSQN2XB7KFWHpx-R6gLLEaWQju1GRna4F0lMoQY6qRqvJg-8fagIFW5vqNE1Q8oJz5hB57uUmeropDna~lNQmiDs-v2xcA6eblKONdOEL3Aj3U6gm1U6-KlEtp-fxtrjVTnCUCqOm9iUunEaNEwaqBoOMWXNSi4mGLoIFQytwdq8zm2Xco85AYjXCocy0I8odKBkLhFhcps5PEXCmqXkLmhuw__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA);
- background-size: 800px;
- background-repeat: no-repeat;
- background-position: center;
+  background-size: 550px;
+  background-repeat: no-repeat;
+  background-position: top;
 
   .button__container {
     width: auto;
@@ -28,14 +28,12 @@ export const HomeStyle = styled.main`
     justify-content: space-evenly;
     flex-wrap: wrap;
     position: absolute;
-margin-top: 100px;
-    left: 13%;
-  
-    
-    h1, span{
-      left:500px ;
+    margin-top: 50px;
+    left: 5%;
 
-
+    h1,
+    span {
+      left: 400px;
     }
   }
 
@@ -71,8 +69,7 @@ margin-top: 100px;
     background-repeat: no-repeat;
     opacity: 0.5; */
 
-    
-  /* } */ 
+  /* } */
   /* .div__background--3 {
     width: 100%;
     height: 100%;
@@ -81,9 +78,6 @@ margin-top: 100px;
     background-repeat: no-repeat;
     */
 
-  
-   
-  
   .button__candidate--register {
     background-color: #23ce6b;
     border: none;
@@ -115,5 +109,103 @@ margin-top: 100px;
     transition: 0.6s;
     cursor: pointer;
     font-family: "VT323", monospace;
+  }
+
+  @media (max-width: 755px) {
+    width: 100%;
+
+    background-color: #002a32;
+    font-family: "VT323", monospace;
+    background-image: url(https://s3-alpha-sig.figma.com/img/83f4/a1d3/8642817e7d3ef59623c7160eca36401f?Expires=1658707200&Signature=ddK62VN2DvhJiGyQkq9STInfbcl-zptwhTp39RZ5dJTzPd0IOvvsbSTsEv9FUe4~-zSkRYhMoeah8K-DGxSbEGqn7~CWXQfBlb8syJX7U5ljWkUGzObPHifZWt-lbJSQN2XB7KFWHpx-R6gLLEaWQju1GRna4F0lMoQY6qRqvJg-8fagIFW5vqNE1Q8oJz5hB57uUmeropDna~lNQmiDs-v2xcA6eblKONdOEL3Aj3U6gm1U6-KlEtp-fxtrjVTnCUCqOm9iUunEaNEwaqBoOMWXNSi4mGLoIFQytwdq8zm2Xco85AYjXCocy0I8odKBkLhFhcps5PEXCmqXkLmhuw__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA);
+    background-size: 425px;
+    background-repeat: no-repeat;
+    background-position: 0px 42px;
+
+    .button__container {
+      width: auto;
+      display: flex;
+      justify-content: space-evenly;
+      flex-wrap: wrap;
+      position: absolute;
+      margin-top: 110%;
+      right: 0;
+      gap: 24px;
+    }
+    .div__title--container {
+      width: auto;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-evenly;
+      flex-wrap: wrap;
+      position: absolute;
+      margin-top: 10px;
+      left: 13%;
+
+      h1,
+      span {
+        left: 250px;
+      }
+    }
+
+    .h2__title--1 {
+      display: flex;
+      flex-direction: column;
+
+      width: 35%;
+      color: #23ce6b;
+      font-size: 20px;
+      gap: 15px;
+    }
+    .h2__title--2 {
+      display: flex;
+      flex-direction: column;
+
+      width: 45%;
+      color: #f08700;
+      font-size: 20px;
+      gap: 15px;
+    }
+
+    .button__candidate--register {
+      background-color: #23ce6b;
+      border: none;
+      color: white;
+      font-size: 30px;
+      &:hover {
+        background: #39ff14;
+        box-shadow: 0 0 50px #39ff14;
+        font-size: 50px;
+      }
+    }
+    .button__company--register {
+      background-color: #f08700;
+      border: none;
+      color: white;
+      font-size: 30px;
+      &:hover {
+        background: #ffc42c;
+        box-shadow: 0 0 50px #ffc42c;
+        font-size: 50px;
+      }
+    }
+    h1 {
+      font-size: 35px;
+      font-family: "VT323";
+      color: white;
+    }
+    button {
+      transition: 0.6s;
+      cursor: pointer;
+      font-family: "VT323", monospace;
+      width: 160px;
+      height: 40px;
+      margin-left: auto;
+      margin-right: auto;
+      border-radius: 1px;
+      color: white;
+      font-size: 25px;
+      box-sizing: border-box;
+      box-shadow: 4px 4px 0px #000000;
+    }
   }
 `;
